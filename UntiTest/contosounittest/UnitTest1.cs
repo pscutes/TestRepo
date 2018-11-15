fa5e1667e84c5521cd812929292604d13b262dec
Divya Singarapu <Divya.Singarapu_HCL@kantar.com>
2018-11-15 17:19:41 +0530

    firstcommit

---
+++ UntiTest/contosounittest/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;namespace contosounittest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            IWebDriver driver = new ChromeDriver(@"C:\");
+            driver.Manage().Window.Maximize();
+            driver.Navigate().GoToUrl("http://localhost:41787/");
+            //WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromMinutes(1));
+            
+            
+        }
+    }
+}
